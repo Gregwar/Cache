080282baf1f4b55fcf70ed820c2084515ea6ecbf
Gregwar <g.passault@gmail.com>
2014-02-16 11:50:06 +0100

    demo: Using getOrCreateFile instead of passing manually $file to true

--- demo/red-file.php
+++ demo/red-file.php
@@ -9,12 +9,12 @@ $cache->setCacheDirectory('cache'); // This is the default
 
 // If the cache exists, this will return it, else, the closure will be called
 // to create this image
-$file = $cache->getOrCreate('red-square.png', array(), function($filename) {
+$file = $cache->getOrCreateFile('red-square.png', array(), function($filename) {
     $i = imagecreatetruecolor(100, 100);
     imagefill($i, 0, 0, 0xff0000);
     file_put_contents($filename, 'abc');
     imagepng($i, 'a.png');
     imagepng($i, $filename);
-}, true);
+});
 
 echo $file, "\n";
